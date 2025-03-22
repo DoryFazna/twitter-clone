d8ed9404e6e2367706cdd21a467866c8637a7856
DoryFazna <doryfazna@gmail.com>
2025-03-22 04:01:27 +0000

    Add auth implementation

--- backend/server.js
+++ backend/server.js
@@ -2,15 +2,20 @@ import express from "express";
 import authRoutes from './routes/auth.routes.js';
 import dotenv from 'dotenv'
 import connectMongoDB from './db/connectMongoDB.js'
+import cookieParser from "cookie-parser";
 
 dotenv.config();
-
 const app = express();
+
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser())
 
 app.use("/api/auth", authRoutes);
 const PORT = process.env.PORT || 5000;
 
-app.listen(8000, () => {
+app.listen(PORT, () => {
     console.log(`server is running on port ${PORT}`)
     connectMongoDB();
 })
