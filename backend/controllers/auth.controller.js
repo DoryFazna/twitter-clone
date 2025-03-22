eca97c9ac983fb9e13b2017f855983d6b0377032
DoryFazna <doryfazna@gmail.com>
2025-03-22 02:38:40 +0000

    Setup DB and folder structure

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,17 @@
+export const signup = async  (req,res) => {
+    res.json({
+        data:"Youve hit the signup"
+    });
+}
+
+export const login = async  (req,res) => {
+    res.json({
+        data:"Youve hit the signup"
+    });
+}
+
+export const logout = async  (req,res) => {
+    res.json({
+        data:"Youve hit the signup"
+    });
+}
